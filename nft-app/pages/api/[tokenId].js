eb1c9a91cbe258e176e6360c245020681a5669b1
faith-ware <faithade69@gmail.com>
2022-04-10 18:51:53 -0500

    Added the image url to the api

--- nft-app/pages/api/[tokenId].js
+++ nft-app/pages/api/[tokenId].js
@@ -3,14 +3,14 @@ export default function handler(req, res) {
   const tokenId = req.query.tokenId;
   // As all the images are uploaded on github, we can extract the images from github directly.
   const image_url =
-    "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+    "https://raw.githubusercontent.com/faith-ware/faith-arts-nft/main/images/";
   // The api is sending back metadata for a Crypto Dev
   // To make our collection compatible with Opensea, we need to follow some Metadata standards
   // when sending back the response from the api
   // More info can be found here: https://docs.opensea.io/docs/metadata-standards
   res.status(200).json({
-    name: "Crypto Dev #" + tokenId,
-    description: "Crypto Dev is a collection of developers in crypto",
-    image: image_url + tokenId + ".svg",
+    name: "Faith Arts #" + tokenId,
+    description: "Faith Artworks is an NFT collection for people that love artworks.",
+    image: image_url + tokenId + ".jpg",
   });
 }
